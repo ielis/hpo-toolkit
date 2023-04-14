f2b011386f2e71c44b49c8c538aa928e55ad2277
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-04-14 09:47:36 -0400

    Update `SimpleHpoaDiseaseLoader` to parse the novel HPO annotation file.

--- tests/annotations/load/test_hpoa.py
+++ tests/annotations/load/test_hpoa.py
@@ -6,32 +6,56 @@ import hpotk
 from pkg_resources import resource_filename
 
 TOY_HPO = resource_filename(__name__, os.path.join('../../data', 'hp.toy.json'))
-TOY_HPOA = resource_filename(__name__, os.path.join('../../data', 'phenotype.fake.hpoa'))
+TOY_HPOA_OLDER = resource_filename(__name__, os.path.join('../../data', 'phenotype.fake.older.hpoa'))
+TOY_HPOA = resource_filename(__name__, os.path.join('../../data', 'phenotype.fake.novel.hpoa'))
 
 
-class TestHpoaLoader(unittest.TestCase):
-
+class TestHpoaLoaderBase(unittest.TestCase):
     HPO: hpotk.ontology.MinimalOntology
 
     @classmethod
     def setUpClass(cls) -> None:
         cls.HPO = hpotk.ontology.load.obographs.load_minimal_ontology(TOY_HPO)
 
-    def setUp(self) -> None:
-        self.loader = hpotk.annotations.load.hpoa.SimpleHpoaDiseaseLoader(self.HPO)
 
-    def test_toy_phenotypic_features(self):
+class TestHpoaLoader(TestHpoaLoaderBase):
+
+    def setUp(self) -> None:
+        self.loader = hpotk.annotations.load.hpoa.SimpleHpoaDiseaseLoader(TestHpoaLoaderBase.HPO)
+
+    def test_load_hpo_annotations(self):
         diseases = self.loader.load(TOY_HPOA)
         self.assertIsInstance(diseases, hpotk.annotations.HpoDiseases)
 
         self.assertEqual(2, len(diseases))
         self.assertSetEqual({'ORPHA:123456', 'OMIM:987654'}, set(map(lambda d: d.value, diseases.disease_ids)))
+        self.assertEqual(diseases.version, '2021-08-02')
 
-        omim = diseases['OMIM:987654']
+    def test_load_older_hpo_annotations(self):
+        diseases = self.loader.load(TOY_HPOA_OLDER)
+        self.assertIsInstance(diseases, hpotk.annotations.HpoDiseases)
+
+        self.assertEqual(2, len(diseases))
+        self.assertSetEqual({'ORPHA:123456', 'OMIM:987654'}, set(map(lambda d: d.value, diseases.disease_ids)))
+
+
+class TestHpoaDiseaseProperties(TestHpoaLoaderBase):
+    LOADER: hpotk.annotations.load.hpoa.SimpleHpoaDiseaseLoader
+    HPO_DISEASES: hpotk.annotations.HpoDiseases
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        TestHpoaLoaderBase.setUpClass()
+        cls.LOADER = hpotk.annotations.load.hpoa.SimpleHpoaDiseaseLoader(TestHpoaLoaderBase.HPO)
+        cls.HPO_DISEASES = cls.LOADER.load(TOY_HPOA)
+
+    def test_hpoa_disease_properties(self):
+        omim = TestHpoaDiseaseProperties.HPO_DISEASES['OMIM:987654']
         self.assertEqual('Made-up OMIM disease, autosomal recessive', omim.name)
         self.assertEqual(2, len(omim.annotations))
 
-        omim_annotations: typing.List[hpotk.annotations.HpoDiseaseAnnotation] = list(sorted(omim.annotations, key=lambda a: a.identifier.value))
+        omim_annotations: typing.List[hpotk.annotations.HpoDiseaseAnnotation] = list(
+            sorted(omim.annotations, key=lambda a: a.identifier.value))
         first = omim_annotations[0]
         self.assertEqual(first.identifier.value, 'HP:0001167')
         self.assertEqual(first.is_present(), True)
@@ -44,9 +68,5 @@ class TestHpoaLoader(unittest.TestCase):
         self.assertEqual(second.identifier.value, 'HP:0001238')
         self.assertEqual(second.is_absent(), True)
         self.assertEqual(second.ratio.numerator, 0)
-        self.assertEqual(second.ratio.denominator, self.loader.cohort_size)
+        self.assertEqual(second.ratio.denominator, TestHpoaDiseaseProperties.LOADER.cohort_size)
         self.assertEqual(len(second.references), 1)
-
-    def test_disease_data_has_version(self):
-        diseases: hpotk.annotations.HpoDiseases = self.loader.load(TOY_HPOA)
-        self.assertEqual(diseases.version, '2021-08-02')
