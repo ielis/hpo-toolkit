382579b9d8211e88b1020eb45879c4399de2017f
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-05-09 22:30:24 -0400

    Export the most commonly used data model classes at the top-level.

--- src/hpotk/__init__.py
+++ src/hpotk/__init__.py
@@ -1,10 +1,14 @@
 __version__ = "0.1.5dev0"
 
-from . import model
-from . import constants
-from . import graph
-from . import ontology
 from . import algorithm
 from . import annotations
-from . import validate
+from . import constants
+from . import graph
+from . import model
+from . import ontology
 from . import util
+from . import validate
+
+from .graph import OntologyGraph, GraphAware
+from .model import TermId, Term, MinimalTerm, Synonym, SynonymType, SynonymCategory
+from .ontology import Ontology, MinimalOntology
