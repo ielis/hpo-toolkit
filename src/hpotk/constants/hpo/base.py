aa467ab64e069363bc33daec17b3b916844ca2b5
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-10-13 11:15:24 -0400

    Improve `hpotk.constants` docs.

--- src/hpotk/constants/hpo/base.py
+++ src/hpotk/constants/hpo/base.py
@@ -3,8 +3,20 @@ from hpotk.model import TermId
 # Children of All `HP:0000001`, the root of the HPO hierarchy.
 
 PHENOTYPIC_ABNORMALITY: TermId = TermId.from_curie('HP:0000118')
+"""
+Phenotypic abnormality is the parent term of all phenotypic features of HPO.
+"""
+
 CLINICAL_MODIFIER: TermId = TermId.from_curie("HP:0012833")
+"""
+Clinical modifier is the parent term of the clinical modifier HPO module.
+"""
+
 MODE_OF_INHERITANCE: TermId = TermId.from_curie("HP:0000005")
+"""
+Mode of inheritance is the parent term of the inheritance mode subgraph.
+"""
+
 PAST_MEDICAL_HISTORY: TermId = TermId.from_curie("HP:0032443")
 BLOOD_GROUP: TermId = TermId.from_curie("HP:0032223")
 FREQUENCY: TermId = TermId.from_curie("HP:0040279")
