d338d92dbf3393bfe50c686cacfe10d7f0d91c99
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-12-15 11:15:20 -0500

    Implement edge distance based hierarchical term id sorting.

--- src/hpotk/util/sort/_hierarchical.py
+++ src/hpotk/util/sort/_hierarchical.py
@@ -1,4 +1,6 @@
+import abc
 import typing
+import warnings
 
 import numpy as np
 
@@ -6,6 +8,27 @@ from hpotk.graph import OntologyGraph, GraphAware
 from hpotk.model import TermId, Identified
 
 from ._api import TermIdSorting
+
+
+def to_term_id(item: typing.Union[TermId, Identified]) -> TermId:
+    """
+    Private utility method for converting the input into a `TermId`.
+    """
+    if isinstance(item, TermId):
+        return item
+    elif isinstance(item, Identified):
+        return item.identifier
+    else:
+        raise ValueError(f'Item {item} is not `TermId` or `Identified` but it is {type(item)}')
+
+
+def to_ontology_graph(hpo: typing.Union[GraphAware, OntologyGraph]) -> OntologyGraph:
+    if isinstance(hpo, OntologyGraph):
+        return hpo
+    elif isinstance(hpo, GraphAware):
+        return hpo.graph
+    else:
+        raise ValueError(f'`hpo` must be an instance of `OntologyGraph` or `GraphAware` but was {type(hpo)}')
 
 
 class Node(Identified):
@@ -51,28 +74,137 @@ class Node(Identified):
                 f"right={self._right})")
 
 
-class HierarchicalSimilaritySorting(TermIdSorting):
+class SimilarityMeasure(metaclass=abc.ABCMeta):
+    # Private API
+
+    @abc.abstractmethod
+    def compute_similarity(self, left: typing.Union[TermId, Identified],
+                           right: typing.Union[TermId, Identified]) -> typing.Tuple[float, TermId]:
+        """
+        Compute similarity of two term IDs along with the most specific node.
+
+        :param left: an ontology node.
+        :param right: other ontology node.
+        :return: a tuple with node similarity and the most specific node
+        """
+        pass
+
+
+class IcSimilarityMeasure(SimilarityMeasure):
     """
-    `HierarchicalSimilaritySorting` uses hierarchical clustering to sort the term IDs.
-    The clustering uses Resnik term similarity to assess term similarity.
-
-    Notes:
-
-    - Does *not* maintain order if the input is already sorted.
-
-    :param hpo: HPO ontology graph or a graph aware instance.
-    :param ic_source: a callable for getting the information content (IC) as a `float` for a term ID.
+    `IcSimilarityMeasure` uses information content of the most informative common ancestor as the similarity measure
+    and MICA as the most specific node.
     """
+    # Private API
 
     def __init__(self, hpo: typing.Union[OntologyGraph, GraphAware],
                  ic_source: typing.Callable[[TermId], float]):
-        if isinstance(hpo, OntologyGraph):
-            self._hpo = hpo
-        elif isinstance(hpo, GraphAware):
-            self._hpo = hpo.graph
-        else:
-            raise ValueError(f'`hpo` must be an instance of `OntologyGraph` or `GraphAware` but was {type(hpo)}')
+        self._hpo = to_ontology_graph(hpo)
         self._ic_source = ic_source
+
+    def compute_similarity(self, left: typing.Union[TermId, Identified],
+                           right: typing.Union[TermId, Identified]) -> typing.Tuple[float, TermId]:
+
+        # Find the common ancestors of `a` and `b` and find the most informative common ancestor
+        # along with its information content.
+        a_anc = set(self._hpo.get_ancestors(left, include_source=True))
+        b_anc = set(self._hpo.get_ancestors(right, include_source=True))
+        common_ancestors = a_anc.intersection(b_anc)
+
+        return max(
+            map(lambda t: (self._ic_source(t), t), common_ancestors),
+            key=lambda tup: tup[0],  # Order by similarity
+            default=(0., None)
+        )
+
+
+class EdgeSimilarityMeasure(SimilarityMeasure):
+    """
+    `EdgeSimilarityMeasure` uses the reciprocal of the edge distance as a similarity and most specific common ancestor
+    as the most specific node.
+
+    Note, if two paths exist between the query nodes through two separate nodes that also happen to be the most specific
+    common ancestors, then one of them is chosen at random.
+    """
+    # Private API
+
+    def __init__(self, hpo: typing.Union[OntologyGraph, GraphAware]):
+        self._hpo = to_ontology_graph(hpo)
+
+    def compute_similarity(self, left: typing.Union[TermId, Identified],
+                           right: typing.Union[TermId, Identified]) -> typing.Tuple[float, TermId]:
+        dist, node = self.calculate_edge_distance(left, right)
+
+        sim = 1 if dist == 0 else 1 / dist
+
+        return sim, node
+
+    def calculate_edge_distance(self, left: TermId, right: TermId) -> typing.Tuple[int, TermId]:
+        left, right = to_term_id(left), to_term_id(right)
+        if left == right:
+            # Distance to self is `0`.
+            return 0, left
+
+        left_dist = self._get_ancestor_distances(left)
+        right_dist = self._get_ancestor_distances(right)
+
+        return self._find_minimum_distance(left_dist, right_dist)
+
+    def _get_ancestor_distances(self, src: TermId) -> typing.Mapping[TermId, int]:
+        distances = {}
+        seen = set()
+
+        stack = [(0, src)]
+        while stack:
+            distance, term_id = stack.pop()
+
+            current = distance + 1
+            for parent in self._hpo.get_parents(term_id):
+                if parent not in seen:
+                    stack.append((current, parent))
+                seen.add(parent)
+
+            if term_id in distances:
+                # We must keep the shortest distance
+                distances[term_id] = min(distances[term_id], distance)
+            else:
+                distances[term_id] = distance
+
+        return distances
+
+    @staticmethod
+    def _find_minimum_distance(left_dist: typing.Mapping[TermId, int],
+                               right_dist: typing.Mapping[TermId, int]) -> typing.Tuple[int, TermId]:
+        dist = None
+        ancestor = None
+        for shared in left_dist.keys() & right_dist.keys():
+            current = left_dist[shared] + right_dist[shared]
+            if dist is None:
+                dist = current
+                ancestor = shared
+            else:
+                if current < dist:
+                    # Note: we do not update if current = dist. In result, I suspect that the algorithm is not "stable"
+                    # in terms of sorting.
+                    dist = current
+                    ancestor = shared
+
+        return dist, ancestor
+
+
+class HierarchicalSorting(TermIdSorting, metaclass=abc.ABCMeta):
+    """
+    `HierarchicalSorting` implements the hierarchical clustering functionality using
+    given similarity measure.
+    """
+    # Private API
+
+    def __init__(self, hpo: typing.Union[OntologyGraph, GraphAware],
+                 sim_measure: SimilarityMeasure,
+                 epsilon: float = 5e-10):
+        self._hpo = to_ontology_graph(hpo)
+        self._sim_measure = sim_measure
+        self._epsilon = epsilon
 
     def argsort(self, term_ids: typing.Sequence[typing.Union[TermId, Identified]]) -> typing.Sequence[int]:
         if len(term_ids) == 0:
@@ -91,33 +223,30 @@ class HierarchicalSimilaritySorting(TermIdSorting):
     def _hierarchical_cluster(self, nodes: typing.MutableSequence[Node]) -> Node:
         while len(nodes) > 1:
             n_nodes = len(nodes)
-            ics = np.zeros(shape=(n_nodes, n_nodes))
-            micas = np.empty(shape=(n_nodes, n_nodes), dtype=object)
+            sims = np.zeros(shape=(n_nodes, n_nodes))
+            ancestors = np.empty(shape=(n_nodes, n_nodes), dtype=object)
 
             for row in range(0, n_nodes):
-                for col in range(row, n_nodes):
-                    if row == col:
-                        # No self-similarity!
-                        continue
-                    ic, mica = self._get_ic_and_mica(nodes[row], nodes[col])
-                    ics[row, col] = ic
-                    ics[col, row] = ic
-                    micas[row, col] = mica
-                    micas[col, row] = mica
+                for col in range(row + 1, n_nodes):
+                    ic, sim = self._sim_measure.compute_similarity(nodes[row], nodes[col])
+                    sims[row, col] = ic
+                    sims[col, row] = ic
+                    ancestors[row, col] = sim
+                    ancestors[col, row] = sim
 
-            max_row, max_col = np.unravel_index(np.argmax(ics, axis=None), ics.shape)
-            ic_mica = ics[max_row, max_col]
+            max_row, max_col = np.unravel_index(np.argmax(sims, axis=None), sims.shape)
+            sim_anc = sims[max_row, max_col]
 
-            if ic_mica <= 5E-6:
-                # IC is close to zero - should happen when there is no good match left for the remaining nodes,
+            if sim_anc <= self._epsilon:
+                # sim is close to zero - should happen when there is no good match left for the remaining nodes,
                 # and we need to cluster the rest kind of arbitrarily
                 a = nodes.pop()
                 b = nodes.pop()
-                _, mica = self._get_ic_and_mica(a, b)
-                node = Node.merge_nodes(mica, a, b)
+                _, sim = self._sim_measure.compute_similarity(a, b)
+                node = Node.merge_nodes(sim, a, b)
             else:
                 # Let's cluster together the most similar term id pair.
-                max_term_id = micas[max_row, max_col]
+                max_term_id = ancestors[max_row, max_col]
                 a = nodes.pop(max(max_row, max_col))
                 b = nodes.pop(min(max_row, max_col))
                 node = Node.merge_nodes(max_term_id, a, b)
@@ -136,19 +265,14 @@ class HierarchicalSimilaritySorting(TermIdSorting):
         if node.right is not None:
             self._inorder_walk(node.right, collector)
 
-    def _get_ic_and_mica(self, a: typing.Union[TermId, Identified],
-                         b: typing.Union[TermId, Identified]) -> typing.Tuple[float, TermId]:
-        # Find the common ancestors of `a` and `b` and find the most informative common ancestor
-        # along with its information content.
-        a_anc = set(self._hpo.get_ancestors(a, include_source=True))
-        b_anc = set(self._hpo.get_ancestors(b, include_source=True))
-        common_ancestors = a_anc.intersection(b_anc)
-
-        return max(
-            map(lambda t: (self._ic_source(t), t), common_ancestors),
-            key=lambda tup: tup[0],  # Order by similarity
-            default=(0., None)
-        )
+    @staticmethod
+    def _to_term_id(item: typing.Union[TermId, Identified]) -> TermId:
+        if isinstance(item, TermId):
+            return item
+        elif isinstance(item, Identified):
+            return item.identifier
+        else:
+            raise ValueError(f'Item {item} is not `TermId` or `Identified` but it is {type(item)}')
 
     @staticmethod
     def _find_indices(source: typing.Sequence[TermId],
@@ -160,11 +284,70 @@ class HierarchicalSimilaritySorting(TermIdSorting):
 
         return tuple(source.index(s) for s in ordered)
 
-    @staticmethod
-    def _to_term_id(item: typing.Union[TermId, Identified]) -> TermId:
-        if isinstance(item, TermId):
-            return item
-        elif isinstance(item, Identified):
-            return item.identifier
-        else:
-            raise ValueError(f'Item {item} is not `TermId` or `Identified` but it is {type(item)}')
+
+class HierarchicalEdgeTermIdSorting(HierarchicalSorting):
+    """
+    `HierarchicalEdgeTermIdSorting` uses hierarchical clustering to sort the term IDs.
+    The clustering uses edge distance as a proxy to the term similarity.
+
+    Notes:
+
+    - Does *not* maintain order if the input is already sorted.
+
+    :param hpo: HPO ontology graph or a graph aware instance.
+    """
+
+    def __init__(self, hpo: typing.Union[OntologyGraph, GraphAware]):
+        super().__init__(hpo, EdgeSimilarityMeasure(hpo))
+
+
+class HierarchicalIcTermIdSorting(HierarchicalSorting):
+    """
+    `HierarchicalIcTermIdSorting` uses hierarchical clustering to sort the term IDs.
+    The clustering uses Resnik term similarity to assess term similarity.
+
+    Notes:
+
+    - Does *not* maintain order if the input is already sorted.
+
+    :param hpo: HPO ontology graph or a graph aware instance.
+    :param ic_source: a callable for getting the information content (IC) as a `float` for a term ID.
+    """
+
+    def __init__(self, hpo: typing.Union[OntologyGraph, GraphAware],
+                 ic_source: typing.Callable[[TermId], float]):
+        super().__init__(hpo, IcSimilarityMeasure(hpo, ic_source))
+
+    def argsort(self, term_ids: typing.Sequence[typing.Union[TermId, Identified]]) -> typing.Sequence[int]:
+        if len(term_ids) == 0:
+            raise ValueError(f'Term ID sequence must not be empty!')
+
+        term_ids = tuple(to_term_id(item) for item in term_ids)
+        nodes = list(Node.make_tagged_node(tid) for tid in term_ids)
+
+        node = self._hierarchical_cluster(nodes)
+
+        ordered = []
+        self._inorder_walk(node, ordered)
+
+        return self._find_indices(term_ids, ordered)
+
+
+class HierarchicalSimilaritySorting(HierarchicalIcTermIdSorting):
+    """
+    `HierarchicalSimilaritySorting` uses hierarchical clustering to sort the term IDs.
+    The clustering uses Resnik term similarity to assess term similarity.
+
+    Notes:
+
+    - Does *not* maintain order if the input is already sorted.
+
+    :param hpo: HPO ontology graph or a graph aware instance.
+    :param ic_source: a callable for getting the information content (IC) as a `float` for a term ID.
+    """
+
+    def __init__(self, hpo: typing.Union[OntologyGraph, GraphAware],
+                 ic_source: typing.Callable[[TermId], float]):
+        super().__init__(hpo, ic_source)
+        warnings.warn("HierarchicalSimilaritySorting was deprecated and will be remove in v1.0.0. "
+                      "Use HierarchicalIcTermIdSorting instead", DeprecationWarning, stacklevel=2)
--- src/hpotk/util/sort/_test__hierarchical.py
+++ src/hpotk/util/sort/_test__hierarchical.py
@@ -1,17 +1,8 @@
-import ddt
-import unittest
+import pytest
 
 from hpotk import TermId, OntologyGraph
-
 from hpotk.graph import IncrementalCsrGraphFactory
-
-from ._hierarchical import HierarchicalSimilaritySorting
-
-
-def make_ontology_graph() -> OntologyGraph:
-    factory = IncrementalCsrGraphFactory()
-    _, edges = make_nodes_and_edges()
-    return factory.create_graph(edges)
+from ._hierarchical import HierarchicalIcTermIdSorting, HierarchicalEdgeTermIdSorting, EdgeSimilarityMeasure
 
 
 def make_nodes_and_edges():
@@ -50,15 +41,17 @@ def make_nodes_and_edges():
     return nodes, edges
 
 
-@ddt.ddt
-class TestSimilarityHierarchicalSorting(unittest.TestCase):
-    GRAPH: OntologyGraph
+@pytest.fixture
+def ontology_graph() -> OntologyGraph:
+    factory = IncrementalCsrGraphFactory()
+    _, edges = make_nodes_and_edges()
+    return factory.create_graph(edges)
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.GRAPH = make_ontology_graph()
 
-    def setUp(self) -> None:
+class TestHierarchicalSimilaritySorting:
+
+    @pytest.fixture
+    def ic_sim_sorting(self, ontology_graph: OntologyGraph) -> HierarchicalIcTermIdSorting:
         ics = {
             'HP:1': 0.,
 
@@ -70,26 +63,88 @@ class TestSimilarityHierarchicalSorting(unittest.TestCase):
 
             'HP:03': 2.
         }
+
         def ic_source(term_id: TermId) -> float: return ics.get(term_id.value, 0.)
-        self.instance = HierarchicalSimilaritySorting(self.GRAPH, ic_source)
 
-    @ddt.data(
-        (('HP:021', 'HP:03', 'HP:01', 'HP:02', 'HP:1', 'HP:022', 'HP:0110', 'HP:020'),
-         ('HP:0110', 'HP:01', 'HP:020', 'HP:022', 'HP:02', 'HP:021', 'HP:1', 'HP:03')),
+        return HierarchicalIcTermIdSorting(ontology_graph, ic_source)
 
-        # Does not preserve order at the moment :'(.
-        (('HP:0110', 'HP:01', 'HP:020', 'HP:022', 'HP:02', 'HP:021', 'HP:1', 'HP:03'),
-         ('HP:01', 'HP:0110', 'HP:021', 'HP:02', 'HP:022',  'HP:020', 'HP:03', 'HP:1')),
+    @pytest.mark.parametrize('curies, expected',
+                             [
+                                 (('HP:021', 'HP:03', 'HP:01', 'HP:02', 'HP:1', 'HP:022', 'HP:0110', 'HP:020'),
+                                  ('HP:0110', 'HP:01', 'HP:020', 'HP:022', 'HP:02', 'HP:021', 'HP:1', 'HP:03')),
 
-        (('HP:021',),
-         ('HP:021',))
-    )
-    @ddt.unpack
-    def test_argsort(self, curies, expected):
+                                 # Does not preserve order at the moment :'(.
+                                 (('HP:0110', 'HP:01', 'HP:020', 'HP:022', 'HP:02', 'HP:021', 'HP:1', 'HP:03'),
+                                  ('HP:01', 'HP:0110', 'HP:021', 'HP:02', 'HP:022', 'HP:020', 'HP:03', 'HP:1')),
+
+                                 (('HP:021',),
+                                  ('HP:021',))
+                             ]
+                             )
+    def test_argsort(self, curies, expected, ic_sim_sorting: HierarchicalIcTermIdSorting):
         term_ids = tuple(map(TermId.from_curie, curies))
 
-        term_idxs = self.instance.argsort(term_ids)
+        term_indices = ic_sim_sorting.argsort(term_ids)
 
-        ordered = tuple(term_ids[idx].value for idx in term_idxs)
-        self.assertTupleEqual(expected, ordered)
+        ordered = tuple(term_ids[idx].value for idx in term_indices)
+        assert expected == ordered
 
+
+class TestHierarchicalEdgeTermIdSorting:
+
+    @pytest.fixture
+    def edge_sim_sorting(self, ontology_graph: OntologyGraph) -> HierarchicalEdgeTermIdSorting:
+        return HierarchicalEdgeTermIdSorting(ontology_graph)
+
+    @pytest.mark.parametrize('curies, expected',
+                             [
+                                 (('HP:021', 'HP:03', 'HP:01', 'HP:02', 'HP:1', 'HP:022', 'HP:0110', 'HP:020'),
+                                  ('HP:02', 'HP:021', 'HP:022', 'HP:020', 'HP:1', 'HP:03', 'HP:01', 'HP:0110')),
+
+                                 # Does not preserve order at the moment :'(.
+                                 (('HP:0110', 'HP:01', 'HP:020', 'HP:022', 'HP:02', 'HP:021', 'HP:1', 'HP:03'),
+                                  ('HP:1', 'HP:01', 'HP:03', 'HP:02', 'HP:020', 'HP:022', 'HP:021', 'HP:0110')),
+
+                                 (('HP:021',),
+                                  ('HP:021',))
+                             ]
+                             )
+    def test_argsort(self, curies, expected, edge_sim_sorting: HierarchicalEdgeTermIdSorting):
+        term_ids = tuple(map(TermId.from_curie, curies))
+
+        term_indices = edge_sim_sorting.argsort(term_ids)
+
+        ordered = tuple(term_ids[idx].value for idx in term_indices)
+        assert ordered == expected
+
+
+class TestEdgeSimilarityMeasure:
+
+    @pytest.fixture
+    def edge_sim(self, ontology_graph: OntologyGraph) -> EdgeSimilarityMeasure:
+        return EdgeSimilarityMeasure(ontology_graph)
+
+    @pytest.mark.parametrize('left, right, common_ancestor, expected',
+                             [
+                                 ('HP:1', 'HP:1', 'HP:1', 0),
+                                 ('HP:01', 'HP:01', 'HP:01', 0),
+                                 ('HP:010', 'HP:0110', 'HP:010', 1),
+                                 ('HP:011', 'HP:0110', 'HP:011', 1),
+                                 ('HP:01', 'HP:0110', 'HP:01', 2),
+                                 ('HP:1', 'HP:0110', 'HP:1', 3),
+                                 ('HP:03', 'HP:01', 'HP:1', 2),
+                                 ('HP:03', 'HP:02', 'HP:1', 2),
+                                 ('HP:0110', 'HP:022', 'HP:1', 5),
+                             ])
+    def test_compute_edge_distance(self, left: str, right: str, common_ancestor: str, expected: int,
+                                   edge_sim: EdgeSimilarityMeasure):
+        left = TermId.from_curie(left)
+        right = TermId.from_curie(right)
+        common_ancestor = TermId.from_curie(common_ancestor)
+
+        lr_dist, lr_term_id = edge_sim.calculate_edge_distance(left, right)
+        assert lr_dist == expected
+        rl_dist, rl_term_id = edge_sim.calculate_edge_distance(right, left)
+        assert rl_dist == expected
+        assert lr_term_id == rl_term_id
+        assert lr_term_id == common_ancestor
