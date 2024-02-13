31a66bf22717778cd5aff739097b2eea952f283f
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-02-12 21:33:45 -0500

    Next development iteration `0.4.3dev0`.

--- src/hpotk/__init__.py
+++ src/hpotk/__init__.py
@@ -2,7 +2,7 @@
 HPO toolkit is a library for working with Human Phenotype Ontology and the HPO annotation data.
 """
 
-__version__ = "0.4.2"
+__version__ = "0.4.3dev0"
 
 from . import algorithm
 from . import annotations
