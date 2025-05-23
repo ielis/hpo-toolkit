28436bfd183a550b11d101d9068d26b5bb71c579
Daniel Danis <daniel.gordon.danis@protonmail.com>
2025-05-23 13:42:29 +0200

    Remove redundant exports.

--- src/hpotk/constants/__init__.py
+++ src/hpotk/constants/__init__.py
@@ -1,9 +1,3 @@
 """
 A module with useful constants of the supported ontologies.
 """
-
-from . import hpo
-
-__all__ = [
-    "hpo",
-]
--- src/hpotk/constants/hpo/__init__.py
+++ src/hpotk/constants/hpo/__init__.py
@@ -1,19 +1,3 @@
 """
 A module with commonly used HPO constants.
 """
-
-from . import base
-from . import frequency
-from . import inheritance
-from . import onset
-from . import organ_system
-from . import severity
-
-__all__ = [
-    "base",
-    "frequency",
-    "inheritance",
-    "onset",
-    "organ_system",
-    "severity",
-]
